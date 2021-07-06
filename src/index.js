89f03d39132e19bb5444d418ad1b022fd43dcb78
Thefederico <fedeupe95@gmail.com>
2021-07-05 19:02:31 -0500

    succes alert

--- src/index.js
+++ src/index.js
@@ -2,32 +2,48 @@ const express = require("express");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const path = require("path");
-
+const flash = require("connect-flash");
+const session = require("express-session");
+const mysqlStore = require("express-mysql-session");
+const { database } = require("./keys");
 
 // initializations
 const app = express();
 
 //settings
 app.set("port", process.env.PORT || 4000);
-app.set("views", path.join(__dirname, "views"))
-app.engine(".hbs", exphbs({
-  defaultLayout: "main",
-  layoutsDir: path.join(app.get("views"), "layouts"),
-  partialsDir: path.join(app.get("views"), "components"),
-  extname: ".hbs",
-  helpers: require("./lib/handlebars")
-}));
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "components"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars"),
+  })
+);
 app.set("view engine", ".hbs");
 
 //Middlewares
+app.use(
+  session({
+    secret: "app-links",
+    resave: false,
+    saveUnitialized: false,
+    store: new mysqlStore(database),
+  })
+);
+app.use(flash());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //ENV
 app.use((req, res, next) => {
+  app.locals.success = req.flash("success");
   next();
-})
+});
 
 //Routes
 app.use(require("./routes/routes"));
@@ -35,7 +51,7 @@ app.use(require("./routes/authentication"));
 app.use("/links", require("./routes/links"));
 
 //Public
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 //starting server
 app.listen(app.get("port"), () => {
--- src/routes/links.js
+++ src/routes/links.js
@@ -5,28 +5,47 @@ const db = require("../database");
 
 router.get("/add", (req, res) => {
   res.render("links/add");
-})
+});
 
 router.post("/add", async (req, res) => {
-  const { title, url, description  } = req.body;
+  const { title, url, description } = req.body;
   const newLink = {
     title,
     url,
-    description
+    description,
   };
   await db.query("INSERT INTO links set ?", [newLink]);
-  res.redirect("/links")
-})
+  req.flash("success", "Link added successfully");
+  res.redirect("/links");
+});
 
 router.get("/", async (req, res) => {
   const links = await pool.query("SELECT * FROM links");
   res.render("links/list", { links });
-})
+});
 
 router.get("/delete/:id", async (req, res) => {
   const { id } = req.params;
   await db.query("DELETE FROM links WHERE ID = ?", [id]);
-  res.redirect("/links")
-})
+  res.redirect("/links");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const links = await db.query("SELECT * FROM links WHERE id = ?", [id]);
+  res.render("links/edit", { link: links[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  await db.query("UPDATE links set id = ?", [newLink, id]);
+  res.redirect("/links");
+});
 
 module.exports = router;
