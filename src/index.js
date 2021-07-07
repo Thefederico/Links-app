d53deb2b983bf39e415d69a9a4da19821c73cf33
Thefederico <fedeupe95@gmail.com>
2021-07-06 20:48:21 -0500

    ready signup and signin

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const flash = require("connect-flash");
 const session = require("express-session");
 const mysqlStore = require("express-mysql-session");
 const { database } = require("./keys");
+const passport = require("passport");
 
 // initializations
 const app = express();
+require("./lib/passport");
 
 //settings
 app.set("port", process.env.PORT || 4000);
@@ -38,10 +40,14 @@ app.use(flash());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 //ENV
 app.use((req, res, next) => {
   app.locals.success = req.flash("success");
+  app.locals.success = req.flash("message");
+
   next();
 });
 
